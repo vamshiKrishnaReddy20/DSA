763eaca6d783ab1cfae713024f2dfc803f306314
vamshiKrishnaReddy20 <vamshi172015@outlook.com>
2026-04-17 00:36:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,6 +1,6 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        output = []
+        result = []
         for i in range(1, n+1):
             string = str(i)
             if i % 15 == 0:
@@ -9,5 +9,5 @@ class Solution:
                 string = "Fizz"
             elif i % 5 == 0:
                 string = "Buzz"
-            output.append(string) 
-        return output      
+            result.append(string) 
+        return result      
